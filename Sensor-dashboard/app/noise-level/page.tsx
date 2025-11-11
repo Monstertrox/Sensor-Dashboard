4248f525c46e5cb649aaac86e6d5b5aabec93479
danielosmaa <daniel.osmaa@ecci.edu.co>
2025-11-11 11:58:13 -0500

    Update page.tsx

--- Sensor-dashboard/app/noise-level/page.tsx
+++ Sensor-dashboard/app/noise-level/page.tsx
@@ -1,7 +1,7 @@
 import { Suspense } from "react"
 import NoiseLevelPageContent from "@/components/noise-level-page-content"
 
-export default function NoisePage() {
+export default function NoiseLevelPage() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <NoiseLevelPageContent />
