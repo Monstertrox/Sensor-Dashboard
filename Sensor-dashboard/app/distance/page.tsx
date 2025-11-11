b7dbda5a925dadd23cd2ddd5e63341945be70a05
Juanpis <143646386+JuanpisECCI@users.noreply.github.com>
2025-11-10 19:26:30 -0500

    page.tsx

--- Sensor-dashboard/app/distance/page.tsx
+++ Sensor-dashboard/app/distance/page.tsx
@@ -1,1 +1,10 @@
-ss
+import { Suspense } from "react"
+import DistancePageContent from "@/components/distance-page-content"
+
+export default function DistancePage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <DistancePageContent />
+    </Suspense>
+  )
+}
